85408f2d441ee91abae784075549f3310cde281a
demogorgon22 <sammagnus22@yahoo.com>
2019-01-14 23:30:07 -0500

    Make spirit dragons true dragons.

--- include/mondata.h
+++ include/mondata.h
@@ -289,7 +289,8 @@
 				 (ptr) == &mons[PM_RODENT_OF_UNUSUAL_SIZE])
 #define is_dragon(ptr)		(((ptr)->mflagsa & MA_DRAGON) != 0L)
 #define is_true_dragon(ptr)		((monsndx(ptr) >= PM_BABY_GRAY_DRAGON && monsndx(ptr) <= PM_YELLOW_DRAGON) ||\
-								(ptr) == &mons[PM_PLATINUM_DRAGON] || (ptr) == &mons[PM_CHROMATIC_DRAGON])
+								(ptr) == &mons[PM_PLATINUM_DRAGON] || (ptr) == &mons[PM_CHROMATIC_DRAGON] ||\
+								(ptr) == &mons[PM_EDEN] || (ptr) == &mons[PM_FAFNIR])
 #define is_pseudodragon(ptr)	(monsndx(ptr) >= PM_TINY_PSEUDODRAGON && monsndx(ptr) <= PM_GIGANTIC_PSEUDODRAGON)
 #define is_bird(ptr)		(((ptr)->mflagsa & MA_AVIAN) != 0L)
 #define is_giant(ptr)		(((ptr)->mflagsa & MA_GIANT) != 0L)
